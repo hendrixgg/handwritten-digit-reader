d93df685db7a332265b2fc63aba6e84efacd9b5c
hendrixgg <hendrixg10@gmail.com>
2022-05-19 22:12:00 -0400

    renamed some files and updated .gitignore

--- MiscTests.cpp
+++ MiscTests.cpp
@@ -18,27 +18,27 @@ struct TrainData {
 TrainData data;
 
 int main() {
-    // FILE* data_file =  fopen("./TrainData/TrainData.bin", "rb");
-    // fread(&data, sizeof(TrainData), 1, data_file);
-    // printf("label: %d\n", data.labels[0]);
-    // for(int i = 0; i < 28; ++i) {
-    //     for(int j = 0; j < 28; ++j) {
-    //         printf("%4d", data.images[0][i*28 + j]);
-    //     }
-    //     printf("\n");
-    // }
-    // fclose(data_file);
-
-
-    std::vector<int> myVec({1, 2, 3, 4, 5});
-    FILE* savedVectorW = fopen("savedVector.bin", "wb");
-    fwrite(myVec.data(), sizeof(int), myVec.size(), savedVectorW);
-    fclose(savedVectorW);
-
-    int myData[myVec.size()];
-    FILE* savedVectorR = fopen("savedVector.bin", "rb");
-    fread(myData, sizeof(int), myVec.size(), savedVectorR);
-    for(int i = 0; i < 5; ++i) {
-        printf("%d ", myData[i]);
+    FILE* data_file =  fopen("./TrainData/TrainData.bin", "rb");
+    fread(&data, sizeof(TrainData), 1, data_file);
+    printf("label: %d\n", data.labels[0]);
+    for(int i = 0; i < 28; ++i) {
+        for(int j = 0; j < 28; ++j) {
+            printf("%4d", data.images[0][i*28 + j]);
+        }
+        printf("\n");
     }
+    fclose(data_file);
+
+
+    // std::vector<int> myVec({1, 2, 3, 4, 5});
+    // FILE* savedVectorW = fopen("savedVector.bin", "wb");
+    // fwrite(myVec.data(), sizeof(int), myVec.size(), savedVectorW);
+    // fclose(savedVectorW);
+
+    // int myData[myVec.size()];
+    // FILE* savedVectorR = fopen("savedVector.bin", "rb");
+    // fread(myData, sizeof(int), myVec.size(), savedVectorR);
+    // for(int i = 0; i < 5; ++i) {
+    //     printf("%d ", myData[i]);
+    // }
 }
