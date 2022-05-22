757b5cb19aa79c2680ab71b90e3fee15ab00e779
hendrixgg <hendrixg10@gmail.com>
2022-05-22 18:42:47 -0400

    testing changes

--- NeuralNetwork/testing.cpp
+++ NeuralNetwork/testing.cpp
@@ -13,7 +13,6 @@ struct TestData {
 // NeuralNet digitReader("savedNeuralNetwork.bin");
 NeuralNet digitReader("currentNeuralNetwork.bin");
 
-// Trainer trainer(&digitReader);
 TestData data;
 
 int main() {
@@ -22,57 +21,50 @@ int main() {
     fread(&data, sizeof(TestData), 1, testDatafile);
     fclose(testDatafile);
     
-    char userInput;
-    while (true){
-        double avgCost = 0;
-        int numTests = data.size;
+    double avgCost = 0;
+    const int numTests = data.size, startPos = 0;
+    printf("[Program Start]\n");
+    auto begin = std::chrono::steady_clock::now();
+    
+    int correctAnswers = 0;
+    for(int t = startPos; t < startPos + numTests; ++t) {
+        // print image
+        // printf("label: %d\n", data.labels[t]);
+        // for(int i = 0; i < 28; ++i) {
+        //     for(int j = 0; j < 28; ++j) {
+        //         printf("%4d", data.images[t][i*28 + j]);
+        //     }
+        //     printf("\n");
+        // }
 
-        printf("[Program Start]\n");
-        auto begin = std::chrono::steady_clock::now();
-        
-        for(int t = 0; t < data.size; ++t) {
-            // run test case through network
-            std::vector<double> output(digitReader(std::vector<double>(data.images[t], data.images[t]+784)));
-
-            // find answer
-            double maxVal = -1e9;
-            int answer = 0;
-            for(int i = 0; i < output.size(); ++i) {
-                if(output[i] > maxVal) {
-                    maxVal = output[i], answer = i;
-                }
+        // find the network's answer
+        std::vector<double> output(digitReader(std::vector<double>(data.images[t], data.images[t]+784)));
+        double maxVal = -1e9;
+        int answer = 0;
+        for(int i = 0; i < output.size(); ++i) {
+            if(output[i] > maxVal) {
+                maxVal = output[i], answer = i;
             }
-            std::vector<double> expected(10);
-            expected[data.labels[0]] = 1;
-            avgCost += digitReader.error(expected);
-
-            // // display results
-            // printf("label: %d\n", data.labels[t]);
-            // printf("network output:\n");
-            // for(int i = 0; i < output.size(); ++i) {
-            //     printf("%d: %.2lf %s\n", i, output[i], (i == answer ? "<--" : ""));
-            // }
-            // printf("cost: %lf\n", digitReader.error(expected));
         }
-        
-        auto end = std::chrono::steady_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-        printf("[Time Elasped: %lld ms]\n", duration.count());
-
-        printf("average cost: %lf\n", avgCost/numTests);
-
-        // save the neural network
-        printf("do you want to save the current neural network? This will overwrite the existing save file in this directory.\n");
-        printf("(Y/N, q/Q to quit): ");
-        scanf("%c", &userInput);
-        fflush(stdin);
-        
-        if(userInput == 'q' || userInput == 'Q') 
-            break;
-        if(userInput == 'y' || userInput == 'Y') {
-            digitReader.saveToFile("savedNeuralNetwork.bin");
-        }
-        // digitReader.initRandom();
+        std::vector<double> expected(10);
+        expected[data.labels[t]] = 1;
+        avgCost += digitReader.error(expected);
+        correctAnswers += answer==data.labels[t];
+        // display results
+        // printf("label: %d\n", data.labels[t]);
+        // printf("network output:\n");
+        // for(int i = 0; i < output.size(); ++i) {
+        //     printf("%d: %.2lf %s\n", i, output[i], (i == answer ? "<--" : ""));
+        // }
+        // printf("cost: %lf\n", digitReader.error(expected));
     }
     
+    auto end = std::chrono::steady_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    printf("[Time Elasped: %lld ms]\n", duration.count());
+
+    printf("average cost: %lf\n", avgCost/numTests);
+    printf("%c correct: %lf\n", '%', correctAnswers * 1.0 / numTests);
+    
+    // digitReader.saveToFile("savedNeuralNetwork.bin");
 }
