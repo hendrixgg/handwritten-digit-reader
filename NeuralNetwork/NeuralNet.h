555c5dc2b71e5e339fcbbc7c965b86d31b74b33c
hendrixgg <hendrixg10@gmail.com>
2022-05-21 23:41:48 -0400

    changed NeuralNet constructor and removed inputSize variable

--- NeuralNetwork/NeuralNet.h
+++ NeuralNetwork/NeuralNet.h
@@ -13,7 +13,7 @@ double random(const double& range_from, const double& range_to)
 
 struct NeuralNet {
     // 0-th layer is the output layer, 1-th layer is the last hidden layer, ...
-    int inputSize, numberOfLayers;
+    int numberOfLayers; // input layer + hidden layers + output layer
     std::vector<int> nodesInLayer;
     std::vector<std::vector<double>> z_value;
     std::vector<std::vector<double>> value; // value of a node v[L][i] = value of the i-th node on the L-th layer
@@ -23,7 +23,7 @@ struct NeuralNet {
     std::vector<std::vector<double>> bias;
 
     // constructs a neural net with the specified structure containing random weights and biases
-    NeuralNet(const int inpSize, const int numOfLayers, const std::vector<int>& dimensions): inputSize(inpSize), numberOfLayers(numOfLayers), nodesInLayer(dimensions) {
+    NeuralNet(const std::vector<int>& dimensions): numberOfLayers(dimensions.size()), nodesInLayer(dimensions) {
         // put in a new weight matrix for each layer in the network
         for(int l = 0; l + 1 < numberOfLayers; ++l) {
             value.emplace_back(nodesInLayer[l]);
@@ -37,19 +37,7 @@ struct NeuralNet {
                 bias[l][j] = random(-1, 1);
             }
         }
-        // first hidden layer
-        const int nodesInFirstLayer = nodesInLayer[numberOfLayers - 1];
-        value.emplace_back(nodesInFirstLayer);
-        z_value.emplace_back(nodesInFirstLayer);
-        weight.emplace_back(nodesInFirstLayer, std::vector<double>(inputSize));
-        bias.emplace_back(nodesInFirstLayer);
-        for(int j = 0; j < nodesInFirstLayer; ++j) {
-            for(int k = 0; k < inputSize; ++k) {
-                weight[numberOfLayers - 1][j][k] = random(-1, 1); // random value to be put as a weight
-            }
-            bias[numberOfLayers - 1][j] = random(-1, 1);
-        }
-        value.emplace_back(inputSize); // for input layer
+        value.emplace_back(nodesInLayer[numberOfLayers-1]); // for input layer
     }
 
     // constructs a neural net from the source file containing structure, weights and biases
@@ -57,7 +45,6 @@ struct NeuralNet {
         FILE* sourceFile = fopen(sourceFilePath, "rb");
         
         // read dimesions of neural network
-        fread(&inputSize, sizeof(int), 1, sourceFile);
         fread(&numberOfLayers, sizeof(int), 1, sourceFile);
         nodesInLayer.resize(numberOfLayers);
         fread(nodesInLayer.data(), sizeof(int), numberOfLayers, sourceFile);
@@ -73,17 +60,7 @@ struct NeuralNet {
                 fread(&bias[l][j], sizeof(double), 1, sourceFile);
             }
         }
-        // first hidden layer
-        const int nodesInFirstHiddenLayer = nodesInLayer[numberOfLayers - 1];
-        value.emplace_back(nodesInFirstHiddenLayer);
-        z_value.emplace_back(nodesInFirstHiddenLayer);
-        weight.emplace_back(nodesInFirstHiddenLayer, std::vector<double>(inputSize));
-        bias.emplace_back(nodesInFirstHiddenLayer);
-        for(int j = 0; j < nodesInFirstHiddenLayer; ++j) {
-            fread(&weight[numberOfLayers - 1][j][0], sizeof(double), inputSize, sourceFile);
-            fread(&bias[numberOfLayers - 1][j], sizeof(double), 1, sourceFile);
-        }
-        value.emplace_back(inputSize); // for input layer
+        value.emplace_back(nodesInLayer[numberOfLayers-1]); // for input layer
 
         fclose(sourceFile);
     }
@@ -105,12 +82,11 @@ struct NeuralNet {
         FILE* saveFile = fopen(filePath, "wb");
         
         // write dimensions of neural network
-        fwrite(&inputSize, sizeof(int), 1, saveFile);
         fwrite(&numberOfLayers, sizeof(int), 1, saveFile);
         fwrite(&nodesInLayer[0], sizeof(int), nodesInLayer.size(), saveFile);
 
         // layer
-        for(int l = 0; l < numberOfLayers; ++l) {
+        for(int l = 0; l < numberOfLayers-1; ++l) {
             // node
             for(int j = 0; j < nodesInLayer[l]; ++j) {
                 // weights, bias
@@ -124,23 +100,21 @@ struct NeuralNet {
 
     // given an input vector, returns the values in the last layer of the network
     std::vector<double> operator ()(const std::vector<double>& input) {
-        if(input.size() != inputSize) {
-            printf("ERROR: Input size not valid for neural network. Input an std::vector<double> with size %d. Operation terminated.\n", inputSize);
+        if(input.size() != nodesInLayer[numberOfLayers-1]) {
+            printf("ERROR: Input size not valid for neural network. Input an std::vector<double> with size %d. Operation terminated.\n", nodesInLayer[numberOfLayers-1]);
             return {-999};
         }
         
-        value[numberOfLayers].assign(input.begin(), input.end()); // input layer
-        int l = numberOfLayers-1, previousLayerSize = inputSize;
-        while (l >= 0) {
+        value[numberOfLayers-1].assign(input.begin(), input.end()); // input layer
+        for (int l = numberOfLayers-2; l >= 0; --l) {
             for(int j = 0; j < nodesInLayer[l]; ++j) {
                 double z = bias[l][j];
-                for(int k = 0; k < previousLayerSize; ++k) {
+                for(int k = 0; k < nodesInLayer[l+1]; ++k) {
                     z += weight[l][j][k] * value[l+1][k];
                 }
                 z_value[l][j] = z;
                 value[l][j] = z > 0 ? z : 0;
             }
-            previousLayerSize = nodesInLayer[l--];
         }
 
         return value[0];
--- NeuralNetwork/Trainer.h
+++ NeuralNetwork/Trainer.h
@@ -4,8 +4,8 @@
 class Trainer {
     NeuralNet* net;
 
-    std::vector<std::vector<std::vector<double>>> negWeightGradient;
-    std::vector<std::vector<double>> negBiasGradient;
+    std::vector<std::vector<std::vector<double>>> weightGradient;
+    std::vector<std::vector<double>> biasGradient;
 
 public:
     Trainer(NeuralNet* nn):  net(nn) {
@@ -13,11 +13,10 @@ public:
             printf("ERROR: NeuralNet* is NULL");
             return;
         }
-        int l = 0, previousLayerSize = net->inputSize;
-        while(l < net->numberOfLayers) {
-            negWeightGradient.emplace_back(net->nodesInLayer[l], std::vector<double>(previousLayerSize));
-            negBiasGradient.emplace_back(net->nodesInLayer[l]);
-            previousLayerSize = net->nodesInLayer[l++];
+        
+        for(int l = 0; l + 1 < net->numberOfLayers; ++l) {
+            weightGradient.emplace_back(net->nodesInLayer[l], std::vector<double>(net->nodesInLayer[l + 1]));
+            biasGradient.emplace_back(net->nodesInLayer[l]);
         }
     }
 
@@ -26,6 +25,52 @@ public:
     }
 
     void backProp(const std::vector<double>& example, const std::vector<double>& expected) {
-        
+        // run operation to get values
+        (*net)(example);
+
+        // l = 0:
+        int l = 0;
+        for(int j = 0; j < net->nodesInLayer[l]; ++j) {
+            double tmp_gradient = (net->value[l][j] - expected[j]) * (net->z_value[l][j] > 0);
+            biasGradient[l][j] = tmp_gradient;
+            for(int k = 0; k < net->nodesInLayer[l + 1]; ++k) {
+                weightGradient[l][j][k] = tmp_gradient * (net->value[l + 1][k]);
+            }
+        }
+
+        // l = 1:
+        l = 1;
+        for(int j = 0; j < net->nodesInLayer[l]; ++j) {
+            double tmp_gradient = 0;
+            for(int m = 0; m < net->nodesInLayer[0]; ++m) {
+                tmp_gradient += (net->value[0][m] - expected[m]) * (net->z_value[0][m] > 0) * (net->weight[0][m][j]) * (net->z_value[l][m] > 0);
+            }
+            biasGradient[l][j] += tmp_gradient;
+            for(int k = 0; k < net->nodesInLayer[2]; ++k) {
+                weightGradient[l][j][k] += tmp_gradient * (net->value[l + 1][k]);
+            }
+        }
+
+        // l = 2:
+        l = 2;
+        for(int j = 0; j < net->nodesInLayer[l]; ++j) {
+            // biasGradient[l][j]
+            for(int m0 = 0; m0 < net->nodesInLayer[0]; ++m0) {
+                double tmp_gradient = 0;
+                for(int m1 = 0; m1 < net->nodesInLayer[1]; ++m1) {
+                    tmp_gradient += (net->weight[0][m0][m1]) * (net->z_value[l-1][m1] > 0) * (net->weight[l-1][m1][j]) * (net->z_value[l][j] > 0);
+                }
+                biasGradient[2][j] += (net->value[0][m0] - expected[m0]) * (net->z_value[0][m0] > 0) * tmp_gradient;
+            }
+            for(int k = 0; k < net->nodesInLayer[l + 1]; ++k) {
+                for(int m0 = 0; m0 < net->nodesInLayer[0]; ++m0) {
+                    double tmp_gradient = 0;
+                    for(int m1 = 0; m1 < net->nodesInLayer[1]; ++m1) {
+                        tmp_gradient += (net->weight[0][m0][m1]) * (net->z_value[l-1][m1] > 0) * (net->weight[l-1][m1][j]) * (net->z_value[l][j] > 0) * (net->value[l+1][k]);
+                    }
+                    weightGradient[2][j][k] += (net->value[0][m0] - expected[m0]) * (net->z_value[0][m0] > 0) * tmp_gradient;
+                }
+            }
+        }
     }
 };
