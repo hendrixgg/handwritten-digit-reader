ac3f094f8d1a12a2513236afebd4f9112b07a2b0
hendrixgg <hendrixg10@gmail.com>
2022-05-22 10:59:48 -0400

    training works now with networks with up to 2 hidden layers

--- NeuralNetwork/Trainer.h
+++ NeuralNetwork/Trainer.h
@@ -7,6 +7,8 @@ class Trainer {
     std::vector<std::vector<std::vector<double>>> weightGradient;
     std::vector<std::vector<double>> biasGradient;
 
+    inline double partialA_wto_Z(int l, int j) { return net->value[l][j] * (1 - net->value[l][j]);}
+
 public:
     Trainer(NeuralNet* nn):  net(nn) {
         if(net == NULL) {
@@ -20,8 +22,20 @@ public:
         }
     }
 
-    void train(const std::vector<std::vector<double>>& trainingExamples, const std::vector<std::vector<double>>& expected) {
-
+    void train(const std::vector<std::vector<double>>& trainingExamples, const std::vector<std::vector<double>>& expected, const double rate) {
+        // find gradient
+        for(int i = 0; i < trainingExamples.size(); ++i) {
+            backProp(trainingExamples[i], expected[i]);
+        }
+        // modify weights and biases based on gradients
+        for(int l = 0; l + 1 < net->numberOfLayers; ++l) {
+            for(int j = 0; j < net->nodesInLayer[l]; ++j) {
+                net->bias[l][j] -= rate * biasGradient[l][j] / trainingExamples.size();
+                for(int k = 0; k < net->nodesInLayer[l + 1]; ++k) {
+                    net->weight[l][j][k] -= rate * weightGradient[l][j][k] / trainingExamples.size();
+                }
+            }
+        }
     }
 
     void backProp(const std::vector<double>& example, const std::vector<double>& expected) {
@@ -31,7 +45,7 @@ public:
         // l = 0:
         int l = 0;
         for(int j = 0; j < net->nodesInLayer[l]; ++j) {
-            double tmp_gradient = (net->value[l][j] - expected[j]) * (net->z_value[l][j] > 0);
+            double tmp_gradient = (net->value[l][j] - expected[j]) * partialA_wto_Z(l, j);
             biasGradient[l][j] = tmp_gradient;
             for(int k = 0; k < net->nodesInLayer[l + 1]; ++k) {
                 weightGradient[l][j][k] = tmp_gradient * (net->value[l + 1][k]);
@@ -43,7 +57,7 @@ public:
         for(int j = 0; j < net->nodesInLayer[l]; ++j) {
             double tmp_gradient = 0;
             for(int m = 0; m < net->nodesInLayer[0]; ++m) {
-                tmp_gradient += (net->value[0][m] - expected[m]) * (net->z_value[0][m] > 0) * (net->weight[0][m][j]) * (net->z_value[l][m] > 0);
+                tmp_gradient += (net->value[0][m] - expected[m]) * partialA_wto_Z(0, m) * (net->weight[0][m][j]) * partialA_wto_Z(l, m);
             }
             biasGradient[l][j] += tmp_gradient;
             for(int k = 0; k < net->nodesInLayer[2]; ++k) {
@@ -58,17 +72,17 @@ public:
             for(int m0 = 0; m0 < net->nodesInLayer[0]; ++m0) {
                 double tmp_gradient = 0;
                 for(int m1 = 0; m1 < net->nodesInLayer[1]; ++m1) {
-                    tmp_gradient += (net->weight[0][m0][m1]) * (net->z_value[l-1][m1] > 0) * (net->weight[l-1][m1][j]) * (net->z_value[l][j] > 0);
+                    tmp_gradient += (net->weight[0][m0][m1]) * partialA_wto_Z(l-1, m1) * (net->weight[l-1][m1][j]) * partialA_wto_Z(l, j);
                 }
-                biasGradient[2][j] += (net->value[0][m0] - expected[m0]) * (net->z_value[0][m0] > 0) * tmp_gradient;
+                biasGradient[l][j] += (net->value[0][m0] - expected[m0]) * partialA_wto_Z(0, m0) * tmp_gradient;
             }
             for(int k = 0; k < net->nodesInLayer[l + 1]; ++k) {
                 for(int m0 = 0; m0 < net->nodesInLayer[0]; ++m0) {
                     double tmp_gradient = 0;
                     for(int m1 = 0; m1 < net->nodesInLayer[1]; ++m1) {
-                        tmp_gradient += (net->weight[0][m0][m1]) * (net->z_value[l-1][m1] > 0) * (net->weight[l-1][m1][j]) * (net->z_value[l][j] > 0) * (net->value[l+1][k]);
+                        tmp_gradient += (net->weight[0][m0][m1]) * partialA_wto_Z(l-1, m1) * (net->weight[l-1][m1][j]) * partialA_wto_Z(l,j) * (net->value[l+1][k]);
                     }
-                    weightGradient[2][j][k] += (net->value[0][m0] - expected[m0]) * (net->z_value[0][m0] > 0) * tmp_gradient;
+                    weightGradient[l][j][k] += (net->value[0][m0] - expected[m0]) * partialA_wto_Z(0, m0) * tmp_gradient;
                 }
             }
         }
--- NeuralNetwork/testing.cpp
+++ NeuralNetwork/testing.cpp
@@ -1,6 +1,8 @@
 #include "NeuralNet.h"
+#include "Trainer.h"
 #include <chrono>
 #include <vector>
+#include <cstdio>
 
 struct TestData {
     int size = 10000;
@@ -10,6 +12,7 @@ struct TestData {
 
 // NeuralNet digitReader({10, 16, 16, 784});
 NeuralNet digitReader("savedNeuralNetwork.bin");
+Trainer trainer(&digitReader);
 TestData data;
 
 int main() {
@@ -19,29 +22,34 @@ int main() {
     fclose(testDatafile);
 
     std::vector<double> input(data.images[0], data.images[0]+784);
-    // for(double& d : input) d /= 255; // make pixel values from 0 to 1 not 0 to 255
+    for(double& d : input) d /= 255; // make pixel values from 0 to 1 not 0 to 255
 
-    // run test case through network
-    std::vector<double> output(digitReader(input));    
-    
-    // find answer
-    double maxVal = -1e9;
-    int answer = 0;
-    for(int i = 0; i < output.size(); ++i) {
-        if(output[i] > maxVal) {
-            maxVal = output[i], answer = i;
+    for(int t = 0; t < 2; ++t) {
+        // run test case through network
+        std::vector<double> output(digitReader(input));
+
+
+        // find answer
+        double maxVal = -1e9;
+        int answer = 0;
+        for(int i = 0; i < output.size(); ++i) {
+            if(output[i] > maxVal) {
+                maxVal = output[i], answer = i;
+            }
         }
-    }
 
-    // display results
-    printf("label: %d\n", data.labels[0]);
-    printf("network output:\n");
-    for(int i = 0; i < output.size(); ++i) {
-        printf("%d: %.2lf %s\n", i, output[i], (i == answer ? "<--" : ""));
+        // display results
+        printf("label: %d\n", data.labels[0]);
+        printf("network output:\n");
+        for(int i = 0; i < output.size(); ++i) {
+            printf("%d: %.2lf %s\n", i, output[i], (i == answer ? "<--" : ""));
+        }
+        std::vector<double> expected(10);
+        expected[data.labels[0]] = 1;
+        printf("cost: %lf\n", digitReader.error(expected));
+
+        trainer.train({input}, {expected}, 1);
     }
-    std::vector<double> expected(10);
-    expected[data.labels[0]] = 1;
-    printf("cost: %lf\n", digitReader.error(expected));
 
     // save the neural network
     char wantToSave;
