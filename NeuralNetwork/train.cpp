f4927753752d11c517cdfe71d568fa07f4d1cccd
hendrixgg <hendrixg10@gmail.com>
2022-05-22 22:20:00 -0400

    added option to specify number of training rounds

--- NeuralNetwork/train.cpp
+++ NeuralNetwork/train.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include <cstdio>
 #include <chrono>
-#include <random>
+#include <numeric>
 #include <algorithm>
 
 struct TrainData {
@@ -38,31 +38,39 @@ int main() {
         break;
     }
     
+    int trainingRounds;
+    printf("Enter number of training rounds to run: ");
+    scanf("%d", &trainingRounds);
     int numberOfBatches;
-    printf("Enter number of training batches to run: ");
+    printf("Enter number of batches per round: ");
     scanf("%d", &numberOfBatches);
 
-    printf("\n[Program Start]\n");
-    auto begin = std::chrono::steady_clock::now();
-    
     int batchSize = 100;
-    std::vector<int> shuffle(data.size);
-    std::iota(shuffle.begin(), shuffle.end(), 0);
-    std::random_shuffle(shuffle.begin(), shuffle.end());
-    
-    for(int batch = 0, t = 0; batch < numberOfBatches; ++batch, t+=batchSize) {
-        std::vector<std::vector<double>> trainingBatch(batchSize, std::vector<double>(784));
-        std::vector<std::vector<double>> expectedOutput(batchSize, std::vector<double>(10));
-        for(int i = t; i < t + batchSize; ++i) {
-            trainingBatch[i - t].assign(data.images[shuffle[i]], data.images[shuffle[i]]+784);
-            expectedOutput[i - t][data.labels[shuffle[i]]] = 1.0;
+    numberOfBatches = std::min(numberOfBatches, data.size / batchSize);
+
+    for(int round = 0; round < trainingRounds; ++round) {
+        printf("\n[Round %d Start]\n");
+        auto begin = std::chrono::steady_clock::now();
+        
+        std::vector<int> shuffle(data.size);
+        std::iota(shuffle.begin(), shuffle.end(), 0);
+        std::random_shuffle(shuffle.begin(), shuffle.end());
+
+        for(int batch = 0, t = 0; batch < numberOfBatches; ++batch, t=(t+batchSize)%(data.size)) {
+            std::vector<std::vector<double>> trainingBatch(batchSize, std::vector<double>(784));
+            std::vector<std::vector<double>> expectedOutput(batchSize, std::vector<double>(10));
+            for(int i = t; i < t + batchSize; ++i) {
+                trainingBatch[i - t].assign(data.images[shuffle[i]], data.images[shuffle[i]]+784);
+                expectedOutput[i - t][data.labels[shuffle[i]]] = 1.0;
+            }
+            trainer.train(trainingBatch, expectedOutput, 0.8);
         }
-        trainer.train(trainingBatch, expectedOutput, 0.8);
+
+        auto end = std::chrono::steady_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+        printf("[Time Elasped: %lld ms]\n", duration.count());
+
+        digitReader.saveToFile("currentNeuralNetwork.bin");
     }
 
-    auto end = std::chrono::steady_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    printf("[Time Elasped: %lld ms]\n", duration.count());
-
-    digitReader.saveToFile("currentNeuralNetwork.bin");
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+
+    std::mt19937 g(rd());
+ 
+    std::shuffle(v.begin(), v.end(), g);
